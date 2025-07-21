a5b1d118e80a015bbe984d6356ed4204c80db3cd
coopergraddon <coopergraddon@gmail.com>
2025-07-21 09:09:21 -0700

    Debug Password

--- server/api/login.post.ts
+++ server/api/login.post.ts
@@ -3,6 +3,7 @@ export default defineEventHandler(async (event) => {
   const password = body.password;
   const config = useRuntimeConfig();
   const sitePassword = config.SITE_PASSWORD;
+  console.log('Loaded password:', sitePassword);
   if (password && password === sitePassword) {
     setCookie(event, 'auth', '1', { path: '/', httpOnly: false });
     return { success: true };
