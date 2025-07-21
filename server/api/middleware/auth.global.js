bf821ca7a77f1f999ca0f8cf8b2ef0e7c3579b35
coopergraddon <coopergraddon@gmail.com>
2025-07-21 08:44:51 -0700

    Middleware Fix

--- server/api/middleware/auth.global.js
+++ server/api/middleware/auth.global.js
@@ -1,5 +1,4 @@
 export default defineNuxtRouteMiddleware((to, from) => {
-  if (process.server) return; // Only run on client
   if (to.path === '/login') return;
   const auth = useCookie('auth');
   if (!auth.value) {
