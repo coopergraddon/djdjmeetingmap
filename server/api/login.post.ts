a3547338da07f065d09d381debb91b5a5850b6c2
coopergraddon <coopergraddon@gmail.com>
2025-07-21 09:01:48 -0700

    File Location to Root

--- server/api/login.post.ts
+++ server/api/login.post.ts
@@ -1,7 +1,8 @@
 export default defineEventHandler(async (event) => {
   const body = await readBody(event);
   const password = body.password;
-  const sitePassword = process.env.SITE_PASSWORD;
+  const config = useRuntimeConfig();
+  const sitePassword = config.SITE_PASSWORD;
   if (password && password === sitePassword) {
     setCookie(event, 'auth', '1', { path: '/', httpOnly: false });
     return { success: true };
